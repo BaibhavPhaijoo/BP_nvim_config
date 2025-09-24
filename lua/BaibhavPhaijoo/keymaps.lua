93176d695e3ef3df6e574cc84c88fa523879c3e8
BAIBHAV PHAIJOO <baibhavphaijoo@gmail.com>
2025-09-24 19:16:23 +0545

    add keymap K to show hints and msgs

--- lua/BaibhavPhaijoo/keymaps.lua
+++ lua/BaibhavPhaijoo/keymaps.lua
@@ -1,1 +1,7 @@
--- add your keymaps here
+-- to show hints and messages
+vim.keymap.set(
+    { "n" },                 -- normal mode
+    "K",                     -- key to press
+    vim.lsp.buf.hover,       -- function to call
+    { noremap = true, silent = true }
+)
